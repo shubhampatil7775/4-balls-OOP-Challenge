df2fb7f9bfa5accd4c3f8e9f69ab35e25374809d
shubhampatil7775 <shubhampatil.sp7775@gmail.com>
2022-01-21 11:21:04 +0530

    4 balls OOP solution

---
+++ src/main/java/FourBallsOOP.java
@@ -0,0 +1,71 @@
+import processing.core.PApplet;
+
+
+class BallDimensions {
+
+    public int diameter;
+    public int xCoordinate;
+    public int yCoordinate;
+
+
+    public BallDimensions(int diameter, int xCoordinate, int yCoordinate) {
+        this.diameter = diameter;
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+    }
+}
+
+class CreatingFourBalls extends PApplet{
+
+    BallDimensions[] balls=new BallDimensions[4];
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 380;
+
+    public CreatingFourBalls(int x) {
+
+        for (int iterator = 0; iterator < 4; iterator++) {
+            
+            balls[iterator]=new BallDimensions(15,10,(iterator+1)*HEIGHT/5);
+        }
+    }
+}
+
+public class FourBallsOOP extends CreatingFourBalls {
+
+
+    FourBallsOOP()
+    {
+        super(20);
+    }
+    public static void main(String[] args) {
+        FourBallsOOP fourBallsOOP = new FourBallsOOP();
+        PApplet.main("FourBallsOOP",args);
+
+    }
+
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+
+    @Override
+    public void draw() {
+
+        for (int iterator = 0; iterator < 4; iterator++) {
+
+            drawCircle(balls[iterator].xCoordinate,balls[iterator].yCoordinate,balls[iterator].diameter);
+            balls[iterator].xCoordinate+= iterator+1;
+        }
+
+    }
+
+    private void drawCircle(int xCoordinate, int yCoordinate,int diameter) {
+
+        ellipse(xCoordinate,yCoordinate, diameter,diameter);
+    }
+
+}
+
